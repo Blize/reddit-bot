e12691033a140b2056c17deb54a4e2ddbfc9e5b4
simylein <67859356+simylein@users.noreply.github.com>
2022-06-20 09:30:32 +0200

    chore: fixed spelling

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sqlite3
 # the regex used to find the excuse on the website http://developerexcuses.com
 excuse_regex = "<a .*?>(.*?)</a>"
 
-# the amout of posts to get per fetch
+# the amount of posts to get per fetch
 submission_limit = 5
 
 # the file name with the submissions that have already been replied to
@@ -25,7 +25,7 @@ if "n" in start_script_question:
 connection = sqlite3.connect('subreddits.sqlite')
 cursor = connection.cursor()
 
-# create a table named subreddits if it doesnt already exsits
+# create a table named subreddits if it does not already exist
 cursor.execute("create table if not exists subreddits (id, name, added_by)")
 
 # check if file exists
@@ -33,7 +33,7 @@ if not os.path.isfile(posts_replied_to_file):
     # create new empty list for new posts
     posts_replied_to = []
 else:
-    # open the file in read mode of the exsisting posts
+    # open the file in read mode of the existing posts
     with open(posts_replied_to_file, "r") as f:
 
         # read the replied posts file
@@ -47,11 +47,11 @@ else:
 
 
 # function that fetches a random developer excuse from the website
-def get_random_exuse():
-    # fetch the developer exuses page
+def get_random_excuse():
+    # fetch the developer excuses page
     resp = urllib.request.urlopen('http://developerexcuses.com')
 
-    # read the page and decode with utf-8 for regex ussage
+    # read the page and decode with utf-8 for regex usage
     page = resp.read().decode('utf-8')
 
     # search html response with regex
@@ -70,7 +70,7 @@ def index_exists(list, index):
         return False
 
 
-# look throught the given array of users and return true if the user is in the list
+# look through the given array of users and return true if the user is in the list
 def check_moderator(mods, user):
     for moderator in mods:
         if moderator.name == user:
@@ -80,52 +80,52 @@ def check_moderator(mods, user):
 
 
 # function that returns all subreddits to watch in a list
-def get_subbreddits_to_watch():
-    subbreddits_to_watch = []
+def get_subreddits_to_watch():
+    subreddits_to_watch = []
 
     cursor.execute("select name from subreddits")
 
     data = cursor.fetchall()
 
     for subreddit in data:
-        subbreddits_to_watch.append(subreddit[0])
+        subreddits_to_watch.append(subreddit[0])
 
-    if len(subbreddits_to_watch) < 1:
+    if len(subreddits_to_watch) < 1:
         return "DeveloperExcusesBot"
 
-    return "+".join(subbreddits_to_watch)
+    return "+".join(subreddits_to_watch)
 
 
 # initiate a new reddit bot instance
 reddit = praw.Reddit('bot1')
 
-print("The Bot is now watching " + get_subbreddits_to_watch())
+print("The Bot is now watching " + get_subreddits_to_watch())
 print("")
 
 while True:
     # define the subbreddit the bot should run on
-    subreddit = reddit.subreddit(get_subbreddits_to_watch())
+    subreddit = reddit.subreddit(get_subreddits_to_watch())
 
-    # for every unread message in the indbox
+    # for every unread message in the inbox
     for message in reddit.inbox.unread(limit=None):
 
         # split the message where there is a space
         message_body = message.body.split()
 
-        # check if the message begins with an exlemation mark
+        # check if the message begins with an exclamation mark
         if message.body[0] != "!":
 
-            # mark the message as read so it doesnt get picked up anymore
+            # mark the message as read so it does not get picked up anymore
             message.mark_read()
 
             # reply to the message that the command was invalid
             message.reply(
                 body=message.body + " Is not an valid command, u can use !add [subreddit] and !remove [subreddit] to add the bot")
 
-            # if the message isnt a command stop logic early
+            # if the message is not a command stop logic early
             continue
 
-        # check if first part of message matches a command else repy with info message
+        # check if first part of message matches a command else reply with info message
         if message_body[0] == "!add":
 
             # check if the commands includes a subreddit
@@ -144,9 +144,9 @@ while True:
                 # mark the message as read
                 message.mark_read()
 
-                # reply to messsage with not found warning
+                # reply to message with not found warning
                 message.reply(
-                    body="The Following subbredit could not be found: " + message_body[1])
+                    body="The Following subbreddit could not be found: " + message_body[1])
                 continue
 
             # search the subreddit and check if it exists
@@ -158,29 +158,29 @@ while True:
                     print("added subreddit: " + new_subreddit.display_name +
                           " by: " + message.author.name)
 
-                    # add new subreddit to subbreddit table with colums (id, name, added_by)
+                    # add new subreddit to subbreddit table with columns (id, name, added_by)
                     cursor.execute(
                         "insert into subreddits (id, name, added_by) values (?, ?, ?)", (new_subreddit.name, new_subreddit.display_name, message.author.name))
 
-                    # save changes so they are persistant over sessions
+                    # save changes so they are persistent over sessions
                     connection.commit()
 
                     # mark the message as read
                     message.mark_read()
 
-                    # reply to the messsage with a sucessful response messsage
+                    # reply to the message with a successful response message
                     message.reply(body="Successfully added subbreddit " +
                                   new_subreddit.display_name + " to watchlist!")
 
                 else:
-                    print("user isnt a moderator of the selected sub")
+                    print("user is not a moderator of the selected sub")
 
                     # mark the message as read
                     message.mark_read()
 
-                    # reply to the message with a messsage with a permission warning
+                    # reply to the message with a message with a permission warning
                     message.reply(
-                        body="You arrent a Moderator of the subreddit: " + new_subreddit.display_name + " so u are not allowed to add the bot, please ask an modarator to add the bot!")
+                        body="You are not a Moderator of the subreddit: " + new_subreddit.display_name + " so u are not allowed to add the bot, please ask an moderator to add the bot!")
                     continue
 
         elif message_body[0] == "!remove":
@@ -201,9 +201,9 @@ while True:
                 # mark the message as read
                 message.mark_read()
 
-                # reply to messsage with not found warning
+                # reply to message with not found warning
                 message.reply(
-                    body="The Following subbredit could not be found: " + message_body[1])
+                    body="The Following subbreddit could not be found: " + message_body[1])
                 continue
 
             # search the subreddit and check if it exists
@@ -219,25 +219,25 @@ while True:
                     cursor.execute(
                         "delete from subreddits where id = :subID", {"subID": new_subreddit.name})
 
-                    # save changes so they are persistant over sessions
+                    # save changes so they are persistent over sessions
                     connection.commit()
 
                     # mark the message as read
                     message.mark_read()
 
-                    # reply to the messsage with a sucessful response messsage
+                    # reply to the message with a successful response message
                     message.reply(body="Successfully removed subbreddit " +
                                   new_subreddit.display_name + " from watchlist!")
 
                 else:
-                    print("user isnt a moderator of the selected sub")
+                    print("user is not a moderator of the selected sub")
 
                     # mark the message as read
                     message.mark_read()
 
-                    # reply to the message with a messsage with a permission warning
+                    # reply to the message with a message with a permission warning
                     message.reply(
-                        body="You arrent a Moderator of the subreddit: " + new_subreddit.display_name + " so u are not allowed to remove the bot, please ask an modarator to remove the bot!")
+                        body="You are not a Moderator of the subreddit: " + new_subreddit.display_name + " so u are not allowed to remove the bot, please ask an moderator to remove the bot!")
                     continue
 
         else:
@@ -249,7 +249,7 @@ while True:
     # for every 5 new submissions/posts
     for submission in subreddit.new(limit=submission_limit):
 
-        # if the submission hasnt been replied to
+        # if the submission has not been replied to
         if submission.id not in posts_replied_to:
 
             # check if the submission has one of the following flairs
@@ -259,10 +259,10 @@ while True:
                 submission.upvote()
 
                 # reply with a random developer excuse
-                submission.reply(body=get_random_exuse())
+                submission.reply(body=get_random_excuse())
 
                 # log reply to submission
-                print("Bot replyed to : ", submission.title +
+                print("Bot replied to : ", submission.title +
                       " in " + submission.subreddit.name)
 
                 # add submission id to list
